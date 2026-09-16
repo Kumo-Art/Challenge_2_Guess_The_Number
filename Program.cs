041b1af5c34784bcfe22413a7544dad859601df4
OtherLettuce <sirpickles246@gmail.com>
2026-09-16 13:49:32 -0700

    added review

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿//Brandon Langehennig
 //Challenge 2 Guess the number
 //Make a game with 4 different modes that generates a random number and has the user guess the number which then says if the number guessed is higher or lower than generated number. Make it replayable
-//Peer Reviewer Name:
-//Review:
+//Peer Reviewer Name: Zackary Santos
+//Review: Code works alright, however it does not let me re-enter my answer and I have to start all over again
 
 string play;
 
